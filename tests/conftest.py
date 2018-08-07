e4ef973be9c218edad153b5933165bca88985e69
Tyler Goodlet <tgoodlet@gmail.com>
2018-08-07 14:30:25 -0400

    Add discovery testing
    Add a new test to verify actors register with their `.uid` tuple as
    per #7. Break off relevant "discovery" tests into a new test module.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 """
 ``tractor`` testing!!
 """
+import random
+from functools import partial, wraps
+
 import pytest
 import tractor
+
+
+_arb_addr = '127.0.0.1', random.randint(1000, 9999)
 
 
 def pytest_addoption(parser):
@@ -16,3 +22,25 @@ def loglevel(request):
     level = tractor.log._default_loglevel = request.config.option.loglevel
     yield level
     tractor.log._default_loglevel = orig
+
+
+@pytest.fixture(scope='session')
+def arb_addr():
+    return _arb_addr
+
+
+def tractor_test(fn):
+    """
+    Use:
+
+    @tractor_test
+    async def test_whatever():
+        await ...
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        # __tracebackhide__ = True
+        return tractor.run(
+            partial(fn, *args, **kwargs), arbiter_addr=_arb_addr)
+
+    return wrapper
