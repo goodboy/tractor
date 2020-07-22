efde3a577336d39bf9b514278506042ffc9313db
Tyler Goodlet <jgbt@protonmail.com>
2020-07-22 02:02:20 -0400

    Simplify the `_child.py` script
    We don't really need stdin for anything but passing the entry point and
    detaching it seemed to just cause errors on cancellation teardown.

--- tractor/_child.py
+++ tractor/_child.py
@@ -3,11 +3,4 @@ import trio
 import cloudpickle
 
 if __name__ == "__main__":
-    job = cloudpickle.load(sys.stdin.detach())
-
-    try:
-        result = trio.run(job)
-        cloudpickle.dump(sys.stdout.detach(), result)
-
-    except BaseException as err:
-        cloudpickle.dump(sys.stdout.detach(), err)
+    trio.run(cloudpickle.load(sys.stdin.buffer))
