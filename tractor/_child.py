ef053eb070d2d5ece554e66068d59e2471a217be
Guillermo Rodriguez <guillermor@fing.edu.uy>
2020-07-27 21:05:00 -0300

    Added named arguments to child init, and now passing less of them.

--- tractor/_child.py
+++ tractor/_child.py
@@ -2,52 +2,49 @@ import sys
 import trio
 import argparse
 
+from ast import literal_eval
+
 from ._actor import Actor
 from ._entry import _trio_main
 
 
-"""This is the "bootloader" for actors started using the native trio backend
-added in #128
+"""This is the "bootloader" for actors started using the native trio backend.
 """
+
+def parse_uid(arg):
+	uid = literal_eval(arg)
+	assert len(uid) == 2
+	assert isinstance(uid[0], str)
+	assert isinstance(uid[1], str)
+	return uid
+
+def parse_ipaddr(arg):
+	addr = literal_eval(arg)
+	assert len(addr) == 2
+	assert isinstance(addr[0], str)
+	assert isinstance(addr[1], int)
+	return addr
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("name")
-    parser.add_argument("uid")
-    parser.add_argument("loglevel")
-    parser.add_argument("bind_addr")
-    parser.add_argument("parent_addr")
-    parser.add_argument("arbiter_addr")
+    parser.add_argument("--uid", type=parse_uid)
+    parser.add_argument("--loglevel", type=str)
+    parser.add_argument("--parent_addr", type=parse_ipaddr)
 
     args = parser.parse_args()
 
-    bind_addr = args.bind_addr.split(":")
-    bind_addr = (bind_addr[0], int(bind_addr[1]))
-    
-    parent_addr = args.parent_addr.split(":")
-    parent_addr = (parent_addr[0], int(parent_addr[1]))
-
-    arbiter_addr = args.arbiter_addr.split(":")
-    arbiter_addr = (arbiter_addr[0], int(arbiter_addr[1]))
-
-    if args.loglevel == "None":
-        loglevel = None
-    else:
-        loglevel = args.loglevel
-
     subactor = Actor(
-        args.name,
-        uid=args.uid,
-        loglevel=loglevel,
-        arbiter_addr=arbiter_addr
-    )
-    subactor._spawn_method = "trio"
+        args.uid[0],
+        uid=args.uid[1],
+        loglevel=args.loglevel,
+	    spawn_method="trio"
+	)
 
     _trio_main(
         subactor,
-        bind_addr,
-        parent_addr=parent_addr
+        ("127.0.0.1", 0),
+        parent_addr=args.parent_addr
     )
