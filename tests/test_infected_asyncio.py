a237dcd0202ae525d11cbc80aeaf2a5f0cf301c7
Tyler Goodlet <jgbt@protonmail.com>
2020-12-21 21:08:53 -0500

    Pass func refs

--- tests/test_infected_asyncio.py
+++ tests/test_infected_asyncio.py
@@ -8,7 +8,9 @@ async def sleep_and_err():
     assert 0
 
 
-async def trio_main():
+async def asyncio_actor():
+    assert tractor.current_actor().is_infected_aio()
+
     await tractor.to_asyncio.run_task(sleep_and_err)
 
 
@@ -16,7 +18,7 @@ def test_infected_simple_error(arb_addr):
 
     async def main():
         async with tractor.open_nursery() as n:
-            await n.run_in_actor('asyncio_actor', trio_main, infected_asyncio=True)
+            await n.run_in_actor(asyncio_actor, infected_asyncio=True)
 
     with pytest.raises(tractor.RemoteActorError) as excinfo:
         tractor.run(main, arbiter_addr=arb_addr)
