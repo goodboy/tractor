0e2c2c940c24f6b26539f28a2cf8752e079a68ce
Tyler Goodlet <jgbt@protonmail.com>
2021-01-08 20:45:19 -0500

    Use actual task name lel

--- tractor/_debug.py
+++ tractor/_debug.py
@@ -31,7 +31,6 @@ log = get_logger(__name__)
 
 __all__ = ['breakpoint', 'post_mortem']
 
-
 # placeholder for function to set a ``trio.Event`` on debugger exit
 _pdb_release_hook: Optional[Callable] = None
 
@@ -120,7 +119,7 @@ async def _acquire_debug_lock(uid: Tuple[str, str]) -> AsyncIterator[None]:
     """Acquire a actor local FIFO lock meant to mutex entry to a local
     debugger entry point to avoid tty clobbering by multiple processes.
     """
-    task_name = trio.lowlevel.current_task()
+    task_name = trio.lowlevel.current_task().name
     try:
         log.debug(
             f"Attempting to acquire TTY lock, remote task: {task_name}:{uid}")
