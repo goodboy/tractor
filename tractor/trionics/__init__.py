4114eb1d25141ab41521da9b5fe31b2f6a7b5b90
Tyler Goodlet <jgbt@protonmail.com>
2021-10-04 11:22:10 -0400

    Move broadcast channel parts into trionics

--- tractor/trionics/__init__.py
+++ tractor/trionics/__init__.py
@@ -3,8 +3,11 @@ Sugary patterns for trio + tractor designs.
 
 '''
 from ._mngrs import async_enter_all
+from ._broadcast import broadcast_receiver, BroadcastReceiver
 
 
 __all__ = [
     'async_enter_all',
+    'broadcast_receiver',
+    'BroadcastReceiver',
 ]
