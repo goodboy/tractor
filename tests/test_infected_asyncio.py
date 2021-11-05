299e4192b088e02e5443f620acb62932ecd7bc30
Tyler Goodlet <jgbt@protonmail.com>
2021-11-05 10:42:43 -0400

    Plan asyncio test set

--- tests/test_infected_asyncio.py
+++ tests/test_infected_asyncio.py
@@ -1,7 +1,12 @@
+'''
+The most hipster way to force SC onto the stdlib's "async".
+
+'''
 import asyncio
 
 import pytest
 import tractor
+
 
 async def sleep_and_err():
     await asyncio.sleep(0.1)
@@ -14,7 +19,7 @@ async def asyncio_actor():
     await tractor.to_asyncio.run_task(sleep_and_err)
 
 
-def test_infected_simple_error(arb_addr):
+def test_aio_simple_error(arb_addr):
 
     async def main():
         async with tractor.open_nursery() as n:
@@ -22,3 +27,35 @@ def test_infected_simple_error(arb_addr):
 
     with pytest.raises(tractor.RemoteActorError) as excinfo:
         tractor.run(main, arbiter_addr=arb_addr)
+
+
+def test_aio_cancel_from_trio(arb_addr):
+    ...
+
+
+def test_aio_cancelled_from_aio_causes_trio_cancelled(arb_addr):
+    ...
+
+
+def test_trio_cancels_aio(arb_addr):
+    ...
+
+
+def test_trio_error_cancels_aio(arb_addr):
+    ...
+
+
+def test_basic_interloop_channel_stream(arb_addr):
+    ...
+
+
+def test_basic_interloop_channel_stream(arb_addr):
+    ...
+
+
+def test_trio_cancels_and_channel_exits(arb_addr):
+    ...
+
+
+def test_aio_errors_and_channel_propagates(arb_addr):
+    ...
