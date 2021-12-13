6f94ffc3047c237d3bc77c375290e60ccf5c00d6
Tyler Goodlet <jgbt@protonmail.com>
2021-12-13 13:08:32 -0500

    Re-license code base for distribution under AGPL
    This commit obviously denotes a re-license of all applicable parts of
    the code base. Acknowledgement of this change was completed in #274 by
    the majority of the current set of contributors. From here henceforth
    all changes will be AGPL licensed and distributed. This is purely an
    effort to maintain the same copy-left policy whilst closing the
    (perceived) SaaS loophole the GPL allows for. It is merely for this
    loophole: to avoid code hiding by any potential "network providers" who
    are attempting to use the project to make a profit without either
    compensating the authors or re-distributing their changes.
    
    I thought quite a bit about this change and can't see a reason not to
    close the SaaS loophole in our current license. We still are (hard)
    copy-left and I plan to keep the code base this way for a couple
    reasons:
    
    - The code base produces income/profit through parent projects and is
      demonstrably of high value.
    - I believe firms should not get free lunch for the sake of
      "contributions from their employees" or "usage as a service" which
      I have found to be a dubious argument at best.
    - If a firm who intends to profit from the code base wants to use it
      they can propose a secondary commercial license to purchase with the
      proceeds going to the project's authors under some form of well
      defined contract.
    - Many successful projects like Qt use this model; I see no reason it
      can't work in this case until such a time as the authors feel it
      should be loosened.
    
    There has been detailed discussion in #103 on licensing alternatives.
    The main point of this AGPL change is to protect the code base for the
    time being from exploitation while it grows and as we move into the next
    phase of development which will include extension into the multi-host
    distributed software space.

--- tractor/_streaming.py
+++ tractor/_streaming.py
@@ -1,3 +1,19 @@
+# tractor: structured concurrent "actors".
+# Copyright 2018-eternity Tyler Goodlet.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 Message stream types and APIs.
 
@@ -26,9 +42,10 @@ from .trionics import broadcast_receiver, BroadcastReceiver
 log = get_logger(__name__)
 
 
-# TODO: generic typing like trio's receive channel
-# but with msgspec messages?
-# class ReceiveChannel(AsyncResource, Generic[ReceiveType]):
+# TODO: the list
+# - generic typing like trio's receive channel but with msgspec
+#   messages? class ReceiveChannel(AsyncResource, Generic[ReceiveType]):
+# - use __slots__ on ``Context``?
 
 
 class ReceiveMsgStream(trio.abc.ReceiveChannel):
