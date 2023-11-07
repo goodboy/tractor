ebf9909cc45d3d13b5d30e61024308b1d418e30c
Tyler Goodlet <jgbt@protonmail.com>
2023-11-07 16:45:22 -0500

    Add `open_root_actor(ensure_registry: bool)`
    Allows forcing the opened actor to either obtain the passed registry
    addrs or raise a runtime error.

--- tractor/_root.py
+++ tractor/_root.py
@@ -86,6 +86,10 @@ async def open_root_actor(
     enable_modules: list | None = None,
     rpc_module_paths: list | None = None,
 
+    # NOTE: allow caller to ensure that only one registry exists
+    # and that this call creates it.
+    ensure_registry: bool = False,
+
 ) -> Actor:
     '''
     Runtime init entry point for ``tractor``.
@@ -225,6 +229,12 @@ async def open_root_actor(
     # Create a new local root-actor instance which IS NOT THE
     # REGISTRAR
     if ponged_addrs:
+
+        if ensure_registry:
+            raise RuntimeError(
+                 f'Failed to open `{name}`@{ponged_addrs}: '
+                'registry socket(s) already bound'
+            )
 
         # we were able to connect to an arbiter
         logger.info(
