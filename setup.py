96cdcd8f3902d2abb6fcd736afa4d584bb92df26
Tyler Goodlet <jgbt@protonmail.com>
2025-03-20 14:24:21 -0400

    Pin to exact `trio` version that still has loose egs

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         # trio related
         # proper range spec:
         # https://packaging.python.org/en/latest/discussions/install-requires-vs-requirements/#id5
-        'trio >= 0.24',
+        'trio == 0.24',
 
         # 'async_generator',  # in stdlib mostly!
         # 'trio_typing',  # trio==0.23.0 has type hints!
