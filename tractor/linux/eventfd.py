6646deb7f405be16dd5e4519279eab055b49761d
Guillermo Rodriguez <guillermo@telos.net>
2025-04-02 14:53:15 -0300

    Add LICENSE headers and module docstring on new files

--- tractor/linux/eventfd.py
+++ tractor/linux/eventfd.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
-Linux specifics, for now we are only exposing EventFD
+Expose libc eventfd APIs
 
 '''
 import os
