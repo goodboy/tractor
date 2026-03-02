ac78e2786b5779ce9e7f7273f4d008fecc5ba4b4
goodboy <goodboy_foss@protonmail.com>
2026-03-02 17:44:21 -0500

    Lol fine! bump it a bit more XD

--- tests/test_docs_examples.py
+++ tests/test_docs_examples.py
@@ -126,7 +126,8 @@ def test_example(
         pytest.skip("2-way streaming example requires py3.9 async with syntax")
 
     timeout: float = (
-        30 if ci_env and _non_linux
+        36
+        if ci_env and _non_linux
         else 16
     )
 
